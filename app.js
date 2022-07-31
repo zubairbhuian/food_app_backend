d7e49ea03eb63686db8a8a01ec16c013c0c43900
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-07-31 13:47:25 +0600

    fgdg

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userRouter=require('./routes/user.route')
 const app =express()
 
 app.use(cors())
+app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use("/api/users",userRouter)
@@ -16,14 +17,14 @@ app.use("/api/users",userRouter)
 // api/users:DELETE
 
 app.get('/',(req,res)=>{
-  res.sendFile(__dirname+'/./janina.json')
+  res.sendFile(__dirname+'/./views/index.html')
 })
 //  route not found
-// app.use((req,res,next)=>{
-//     res.status(404).json({
-//      message:'rout not found'
-//     })
-//   })
+app.use((req,res,next)=>{
+    res.status(404).json({
+     message:'rout not found'
+    })
+  })
 //  Server err
   app.use((err,req,res,next)=>{
     res.status(500).json({
