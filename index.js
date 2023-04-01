86042db4f5c75702425839a86d763276b16c3488
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-04-02 05:08:47 +0600

    port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app=require('./app')
-const port=process.env.PORT || 5000
+// const port=process.env.PORT || 5000
 
+const port= 5000
 app.listen(port,()=>{
     console.log(`server is runing:${port} port`);
 })
