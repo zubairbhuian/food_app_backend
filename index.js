ea521bfe923d253c21b3d705c2341f9f15ddd651
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-04-02 13:53:29 +0600

    all cahnge

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const app=require('./app')
-// const port=process.env.PORT || 5000
+const express = require('express');
+const app = express();
 
-const port= 5000
-app.listen(port,()=>{
-    console.log(`server is runing:${port} port`);
-})
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+app.get('/populerfood', (req, res) => {
+  res.sendFile(__dirname + '/jsons/populer_food.json')
+});
+app.get('/recommendatfood', (req, res) => {
+  res.sendFile(__dirname + '/jsons/populer_food.json')
+});
+
+app.get('/download/hospital', (req, res) => {
+  // Set the file path and name
  
+  const filePath = __dirname + '/doc/LearnQuran.apk';
+  const fileName = 'LearnQuran.apk';
+
+  // Use res.download() to download the file
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('File downloaded successfully');
+    }
+  });
+
+});
+
+app.use((req, res, next) => {
+  res.status(404).redirect('/');
+});
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
