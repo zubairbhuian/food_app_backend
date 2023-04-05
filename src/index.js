400d7e0f4fd732183e7478ce74ddae57765865ec
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-04-05 14:36:43 +0600

    mod-repo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port =process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+  console.log(`http://localhost:${port}/`);
 });
