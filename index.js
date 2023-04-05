71c2f3f6c03e3fdd35551b524584b21e9e65bfbc
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-04-06 02:20:35 +0600

    path-right

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const port =process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public'));          
+app.use(express.static('downloads'));          
 
 
+
+
+
+// ! ================Web Page Route =============
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+
+
+// ! ================(API) Food App Route =============
 app.get('/populerfood', (req, res) => {
   res.sendFile(__dirname + '/jsons/populer_food.json')
 });
@@ -16,9 +25,12 @@ app.get('/recommendatfood', (req, res) => {
   res.sendFile(__dirname + '/jsons/recomended_food.json')
 });
 
-app.get('/download/hospital', (req, res) => {
-  // Set the file path and name
- 
+
+
+
+// ! ================ App Download Route =============
+// Food app
+app.get('apps/food-app/download', (req, res) => {
   const filePath = __dirname + '/doc/LearnQuran.apk';
   const fileName = 'LearnQuran.apk';
 
@@ -30,9 +42,25 @@ app.get('/download/hospital', (req, res) => {
       console.log('File downloaded successfully');
     }
   });
+});
+// hospital app
+app.get('apps/hospital/download', (req, res) => {
+  const filePath = __dirname + '/doc/LearnQuran.apk';
+  const fileName = 'LearnQuran.apk';
 
+  // Use res.download() to download the file
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('File downloaded successfully');
+    }
+  });
 });
 
+
+
+// ! ================ Redirec Route =============
 app.use((req, res, next) => {
   res.status(404).redirect('/');
 });
